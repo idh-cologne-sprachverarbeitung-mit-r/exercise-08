5eed9da94a8438756b9e07d821570b77225d2bcf
ariannabienati <abienati@smail.uni-koeln.de>
2020-06-24 13:21:59 +0200

    in f: 28, 48, 85 doc contain also China-related issues?

--- R/exercise.R
+++ R/exercise.R
@@ -4,3 +4,39 @@ readCorpus <- function(dirname="data", fPattern="*.txt", tokenize=TRUE, scanSep=
 		scan(file.path(dirname,x), what=character(), quote=NULL,sep=scanSep)
 	})
 }
+
+readCorpus(tokenize = FALSE) -> file.collection
+
+EUR.search <- function(file.content) {
+  return(grep("EUR [0-9]*", file.content))
+}
+
+article.search <- function(file.content) {
+  return(length(grep("^(A|An) ", file.content))/as.double(length(file.content)))
+}
+
+question.mark.search <- function(file.content) {
+  return(length(grep("\\? $", file.content)))
+}
+
+China.search <- function(file.content) {
+  return(length(grep("(China|Chinese|Beijing)", file.content)) > 0)
+}
+
+EUR.search(file.collection[[10]])
+article.search(file.collection[[10]])
+question.mark.search(file.collection[[10]])
+question.mark.search.perc <- function(file.content) {
+  return(length(grep("\\? $", file.content))/as.double(length(file.content)))
+}
+
+mean(unlist(lapply(file.collection, question.mark.search.perc)))
+
+EUR.search.perc <- function(file.content) {
+  return(length(grep("EUR [0-9]*", file.content))/as.double(length(file.content)))
+}
+
+which.max(unlist(lapply(file.collection, EUR.search.perc)))
+
+readCorpus(tokenize = TRUE) -> file.collection.tokenized
+which(unlist(lapply(file.collection.tokenized, China.search)))
