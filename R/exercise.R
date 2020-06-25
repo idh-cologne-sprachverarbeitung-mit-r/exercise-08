25e9c2006b8cfe6a6b9ac92518fa78f787a00465
chrkell <you@example.com>
2020-06-26 00:23:58 +0200

    missing task e)

--- R/exercise.R
+++ R/exercise.R
@@ -4,3 +4,27 @@ readCorpus <- function(dirname="data", fPattern="*.txt", tokenize=TRUE, scanSep=
 		scan(file.path(dirname,x), what=character(), quote=NULL,sep=scanSep)
 	})
 }
+
+# a)
+doc10 <- readLines("data/F10.txt")
+grep("\\sEUR\\s\\d", doc10)
+
+# b)
+length(grep("(^A\\s|^An\\s)", doc10))/length(doc10)*100
+
+# c)
+doc10 <- scan(file="data/F10.txt", what="character", quote=NULL)
+grep("\\?", doc10)
+
+# d)
+questionMarks <- unlist(lapply(corpus, function(x) {grep("\\?", x)})) 
+numquestionMarks <- length(questionMarks)
+sentences <- unlist(lapply(corpus, function(x) {grep("[.?!]", x)})) 
+numSentences <- length(sentences)
+numquestionMarks/numSentences*100
+
+# e)
+
+
+# f)
+lapply(corpus, function(x) {grep("(China|Beijing|Chinese)", x, value=TRUE)})
